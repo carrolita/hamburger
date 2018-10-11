10a438352232d5e8e0a3646402ca784f71c13cb1
Caroline Granlund <granlundcaroline@gmail.com>
2018-10-11 11:29:34 +0200

    hamburger completed

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -4,11 +4,24 @@ import Grid from "lib/grid"
 import "./style.css"
 
 class Header extends React.Component {
-
+  state = {
+    open: false
+  }
   render() {
     return (
       <header>
+        <div className={`menu ${this.state.open ? 'active' : ''}`}>
+          <a href="http://technigo.io">Technigo</a>
+          <a href="http://technigo.io">Boot Camp</a>
+          <a href="http://technigo.io">Stories</a>
+          <a href="http://technigo.io">About</a>
+        </div>
         <Grid className="header-grid">
+          <div className={`container ${this.state.open ? 'change' : ''}`} onClick={this.handleClick}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
           <a href="http://technigo.io">Technigo</a>
           <a href="http://technigo.io">Boot Camp</a>
           <a href="http://technigo.io">Stories</a>
@@ -18,6 +31,11 @@ class Header extends React.Component {
     )
   }
 
+  handleClick = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
 }
 
 export default Header
